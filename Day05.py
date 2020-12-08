6dc3eb4c40985f9cb58643670811b0da04e20c9d
tearuka <60264231+tearuka@users.noreply.github.com>
2020-12-08 23:09:36 +0100

    Optimize functions for finding seats
    - Function for finding values is written as an independent function, which is then called by find_row and find_column
    - Function for finding missing values in a list is simplified

--- Day05.py
+++ Day05.py
@@ -7,29 +7,19 @@ inp = open("inp/input_05.txt").read().splitlines()
 
 ### Part 1
 
-def find_row(string):
-    val_min = 0
-    val_max = 128
-    for i in range(7):
-        if string[i] == 'F':
-            val_min = val_min
-            val_max = (val_max + val_min - 1)//2
-        elif string[i] == 'B':
-            val_min = (val_max + val_min + 1)//2
-            val_max = val_max
-    return(val_min)
+def find_value(string, val_min, val_max):
+    for c in string:
+        if c in 'FL':
+            val_max = (val_max + val_min) // 2
+        elif c in 'BR':
+            val_min = (val_max + val_min) // 2
+    return val_min
 
+def find_row(string):
+    return find_value(string[:7], 0, 128)
+    
 def find_column(string):
-    val_min = 0
-    val_max = 8
-    for i in range(3):
-        if string[i + 7] == 'L':
-            val_min = val_min
-            val_max = (val_max + val_min - 1)//2
-        elif string[i + 7] == 'R':
-            val_min = (val_max + val_min + 1)//2
-            val_max = val_max
-    return(val_min)
+    return find_value(string[7:], 0, 8)
 
 # find highest seat ID 
 maxi = 0
@@ -53,15 +43,12 @@ for line in inp:
     seat_ID = row * 8 + column
     IDs.append(seat_ID)
 
-# sort ascending
-IDs_sorted = sorted(IDs)
-
-# find missing value in list
 def find_missing(lst):
-    missing_value = [x for x in range(lst[0], lst[-1] + 1) if x not in lst]
-    return(missing_value)
+    for x in range(min(lst),max(lst)):
+        if x not in lst:
+            return x
 
-my_seat = find_missing(IDs_sorted)
+my_seat = find_missing(IDs)
 
-print("Result for part 2: ", my_seat[0])
+print("Result for part 2: ", my_seat)
 
