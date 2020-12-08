1d6927811000da2321a367070b078c3d2a08016e
tearuka <60264231+tearuka@users.noreply.github.com>
2020-12-08 22:41:03 +0100

    Minor syntax changes

--- Day04.py
+++ Day04.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+
 # read input
-
-with open('inp/input_04.txt','r') as file:
-    inp = file.read()
-
-# split input
-passports = inp.split("\n\n")
-# get rid of the extra \n
-passports = [line.replace('\n',' ') for line in passports]
+inp = open("inp/input_04.txt").read().split("\n\n")
+inp = [line.replace('\n',' ') for line in inp]
 
 
 ### Part 1
@@ -25,15 +20,16 @@ def contains_all(string, fields):
 
 
 counter = 0
-for line in passports:
+for line in inp:
     counter += contains_all(line,required_fields)
+    
 print("Result for part 1: ", counter)
 
 
 ### Part 2
 
 # select only the lines that contain all fields, and split them with space separator
-pas = [line.split(' ') for line in passports if contains_all(line,required_fields)==1]
+passports = [line.split(' ') for line in inp if contains_all(line,required_fields) == 1]
 
 # define validity conditions
 byr_min, byr_max = 1920, 2002
@@ -42,14 +38,14 @@ eyr_min, eyr_max = 2020, 2030
 hgt_min_cm, hgt_max_cm = 150, 193
 hgt_min_in, hgt_max_in = 59, 76
 ecl_set = ['amb','blu','brn','gry','grn','hzl','oth']
-# hcl -> a # followed by exactly six characters 0-9 or a-f.
-# pid -> a nine-digit number, including leading zeroes
+import re
+hcl_condition = re.compile(r'[^a-f0-9#]') # hcl -> a # followed by exactly six characters 0-9 or a-f
+pid_condition = re.compile(r'[^0-9]') # pid -> a nine-digit number, including leading zeroes
 
 # define functions for checking the charcater & number conditions
-import re
-def hcl_match(strg, search=re.compile(r'[^a-f0-9#]').search):
+def hcl_match(strg, search=hcl_condition.search):
     return not bool(search(strg))
-def pid_match(strg, search=re.compile(r'[^0-9]').search):
+def pid_match(strg, search=pid_condition.search):
     return not bool(search(strg))
 
 # function for extracting the field contents
@@ -86,10 +82,11 @@ def check_validity(byr,iyr,eyr,hgt,hcl,ecl,pid):
         return(0)
 
 result_count = 0
-for line in pas:
+for line in passports:
     # extract field contents
     byr, iyr, eyr, hgt, hcl, ecl, pid = map(extract_string,required_fields)
     # check if each passport is OK
     result_count += check_validity(byr,iyr,eyr,hgt,hcl,ecl,pid)
+    
 print("Result for part 2: ", result_count)
 
