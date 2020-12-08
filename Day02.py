829650b1081a80b61c85e2e5ba5601117214d053
tearuka <60264231+tearuka@users.noreply.github.com>
2020-12-08 21:25:36 +0100

    Syntax changes
    - Several variables can be defined in the same line
    - XOR operator ^ : if only one of the conditions is satisfied

--- Day02.py
+++ Day02.py
@@ -10,27 +10,19 @@ inp = open("inp/input_02.txt").read().splitlines()
 import re
 counter = 0
 for line in inp:
-    split_text = re.split('-|: | ',line)
-    mini = int(split_text[0])
-    maxi = int(split_text[1])
-    letter = split_text[2]
-    string = split_text[3]
+    mini, maxi, letter, string = re.split('-|: | ',line)
     count = string.count(letter)
-    if count >= mini and count <= maxi:
-        counter = counter + 1
-print(counter)
+    counter += int(mini) <= count <= int(maxi)
+
+print('Result for part 1: ', counter)  # 628
 
 
 ### Part 2
 
 counter = 0
 for line in inp:
-    split_text = re.split('-|: | ',line)
-    pos1 = int(split_text[0])
-    pos2 = int(split_text[1])
-    letter = split_text[2]
-    string = split_text[3]
-    if (string[pos1 - 1] == letter and string[pos2 - 1] != letter) or (string[pos1 - 1] != letter and string[pos2 - 1] == letter):
-        counter = counter + 1
-print(counter)
+    pos1, pos2, letter, string = re.split('-|: | ',line)
+    if (string[int(pos1) - 1] == letter) ^ (string[int(pos2) - 1] == letter):
+        counter += 1
 
+print('Result for part 2: ', counter)  # 705
