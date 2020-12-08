545396173742f1b63e8538ff29e4fc002ba6545e
tearuka <60264231+tearuka@users.noreply.github.com>
2020-12-08 22:19:29 +0100

    Use modulo to handle grid width

--- Day03.py
+++ Day03.py
@@ -8,38 +8,28 @@ inp = open("inp/input_03.txt").read().splitlines()
 
 ### Part 1
 
-# define function for grid expansion
-def expand_grid(data, step_x_max):
-    expand_times = round(step_x_max * (len(data) - 1) / len(data[0])) + 1
-    data_big = [data[i] * expand_times for i in range(len(data))]
-    return(data_big)
-
-# define function for sliding with a toboggan
 def toboggan(data, step_x, step_y):
-    x = 0
-    y = 0
+    x, y = 0, 0
     trees = 0
-    for _ in range((len(data) - 1) // step_y):
-        x = x + step_x
-        y = y + step_y
-        value = data[y][x]
+    while y < len(data) - 1:
+        x += step_x
+        y += step_y
+        value = data[y][x % len(inp[0])]
         trees += value == '#'
     return(trees)
 
-inp_big = expand_grid(inp, 3)
-result_part1 = toboggan(inp_big, 3, 1)
+result_part1 = toboggan(inp, 3, 1)
 
 print("Result for part 1: ", result_part1)  # 145
 
 
 ### Part 2
 
-inp_bigg = expand_grid(inp, 7)
-res1 = toboggan(inp_bigg, 1, 1)
-res2 = toboggan(inp_bigg, 3, 1)
-res3 = toboggan(inp_bigg, 5, 1)
-res4 = toboggan(inp_bigg, 7, 1)
-res5 = toboggan(inp_bigg, 1, 2)
+res1 = toboggan(inp, 1, 1)
+res2 = toboggan(inp, 3, 1)
+res3 = toboggan(inp, 5, 1)
+res4 = toboggan(inp, 7, 1)
+res5 = toboggan(inp, 1, 2)
 
 result_part2 = res1 * res2 * res3 * res4 * res5
 
