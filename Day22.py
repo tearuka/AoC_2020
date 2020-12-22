66911ad3fd91094156e6d5d2b267682a03764c0d
tearuka <60264231+tearuka@users.noreply.github.com>
2020-12-22 19:24:14 +0100

    Syntax changes
    - nicer way to write return
    - for simple lists I can use list.copy() instead of copy.deepcopy(list)

--- Day22.py
+++ Day22.py
@@ -24,18 +24,12 @@ def crab_combat(deck1, deck2):
             deck2.append(card1)
         else:
             print('Both players have the same card')
-    if deck1 != []:
-        return 1
-    else:
-        return 2
+    return 1 if deck1 else 2
 
 
 def calculate_result(winner):
     result = 0
-    if winner == 1:
-        lst = deck1
-    else:
-        lst = deck2
+    lst = deck1 if winner == 1 else deck2
     for i in range(len(lst)):
         result += (len(lst) - i) * lst[i]
     return result
@@ -53,22 +47,19 @@ deck1, deck2 = inp.split('\n\n')
 deck1 = [int(line) for line in deck1.split('\n')[1:]]
 deck2 = [int(line) for line in deck2.split('\n')[1:]]
 
-
-import copy
-
 def beat_the_crab(deck1, deck2):
     visited1 = []
     visited2 = []
     while deck1 and deck2:
         if deck1 in visited1 or deck2 in visited2:
             return 1
-        visited1.append(copy.deepcopy(deck1))
-        visited2.append(copy.deepcopy(deck2))
+        visited1.append(deck1.copy())
+        visited2.append(deck2.copy())
         card1 = deck1.pop(0)
         card2 = deck2.pop(0)
         if len(deck1) >= card1 and len(deck2) >= card2:
-            deck1_ingame = copy.deepcopy(deck1[:card1])
-            deck2_ingame = copy.deepcopy(deck2[:card2])
+            deck1_ingame = deck1[:card1].copy()
+            deck2_ingame = deck2[:card2].copy()
             winner = beat_the_crab(deck1_ingame, deck2_ingame)
         else:
             if card1 > card2:
@@ -81,16 +72,10 @@ def beat_the_crab(deck1, deck2):
         elif winner == 2:
             deck2.append(card2)
             deck2.append(card1)
-    if deck1 != []:
-        return 1
-    else:
-        return 2
+    return 1 if deck1 else 2
 
 
 winner = beat_the_crab(deck1, deck2)
 res2 = calculate_result(winner)
 
 print('Result for part 2: ', res2)  # 32317
-
-
-
